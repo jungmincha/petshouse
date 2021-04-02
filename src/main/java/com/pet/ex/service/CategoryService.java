c1217272425db2b8e3e0dfd1223bb1c1b77efc6f
eril1024 <eril1024@gmail.com>
2021-04-02 17:20:16 +0900

    Merge remote-tracking branch 'origin/eunhee'
    # Conflicts:
    #	src/main/java/com/pet/ex/controller/CategoryController.java
    #	src/main/resources/mapper/CategoryMapper.xml

--- src/main/java/com/pet/ex/service/CategoryService.java
+++ src/main/java/com/pet/ex/service/CategoryService.java
@@ -28,7 +28,7 @@ public interface CategoryService {
 
 	/* public List <GoodsVO> getSmallGoods(); */
 
-	public List<CategoryVO> getSmallGoods(int getCategory_id);
+	public List<BoardVO> getSmallGoods(int getCategory_id);
 
 	public List <BoardVO> gethighprice();
 
