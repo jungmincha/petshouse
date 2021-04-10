650ed211d81e7c9aa0946fc1c1aa98a23d54aab2
EUN-HEE\으니 <ghey6002@naver.com>
2021-04-10 21:02:02 +0900

    은희/sns등록창

--- src/main/java/com/pet/ex/mapper/SnsMapper.java
+++ src/main/java/com/pet/ex/mapper/SnsMapper.java
@@ -16,73 +16,11 @@ import com.pet.ex.page.Criteria;
 @Mapper
 public interface SnsMapper {
 	
-	/* 상품관리 */
-	public List<GoodsVO> getList(Criteria cri);
-	
-	public int getTotalGoods(Criteria cri);
-	
-	public List<GoodsVO> getList2(int category_id);
-	
-	public List<CategoryVO> getCatengoods();
-	
-	public List<CategoryVO> getSort(CategoryVO categoryVO);
-	
-	public GoodsVO getGoods(int goods_id);
-	
-	public List<StockVO> getStock();
-	
-	public List<CategoryVO> getCategory();
-	
-	public void goodsInput(GoodsVO goodsVO);
-	
-	public void goodsModify(GoodsVO goodsVO);
+	 
 
-	/* public void reviewDelete(int goods_id); */
+	public void snsInput(BoardVO boardVO);
 
-	public void boardDelete(int goods_id);
-
-	public void goodsDelete(int goods_id);
-
-	
-	/* 상품게시글관리 */
-	public List<GoodsVO> getNboard();
-	
-	public void boardInput(BoardVO boardVO);
-
-	public void detailInput(ImageVO imageVO);
-	
-	public BoardVO getboardInfo(int board_id);
-
-	public BoardVO getBoard(int board_id);
-
-	public List<ImageVO> getImg(int goods_id);
-	
-	public BoardVO getRateone(int goods_id);
-
-	public List<CategoryVO> getcateBoard();
-
-	public List<CategoryVO> getsortBoard(CategoryVO categoryVO);
-	
-	
-
-
-	
-
-
- 
-	
-	/* 회원관리 */
-	public List<MemberVO> getMemberlist(Criteria cri);
-
-	public int getMembertotal();
-
-	public MemberVO getMemberdetail(String member_id);
-
-	public void memberDelete(String member_id);
-
-	public List<String> getGoodsSize(int goods_id);
-
-	public void updateCheck(BoardVO boardVO);
+	public void imgInput(ImageVO imageVO);
  
 
 
--- src/main/java/com/pet/ex/service/SnsService.java
+++ src/main/java/com/pet/ex/service/SnsService.java
@@ -1,10 +1,16 @@
 package com.pet.ex.service;
 
- 
+import com.pet.ex.vo.BoardVO;
+import com.pet.ex.vo.ImageVO;
 
 public interface SnsService {
 
- 
+	public void snsInput(BoardVO boardVO, ImageVO imageVO);
+
+	/*
+	 * public void imgInput(ImageVO imageVO);
+	 * 
+	 */
  
 	
 	
