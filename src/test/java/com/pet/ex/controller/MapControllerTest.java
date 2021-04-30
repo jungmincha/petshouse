38e03d72ce52f2c2f3e524b16606db2969ba6714
jungmin <prttyjimmy@naver.com>
2021-04-30 09:54:21 +0900

    정민/단위테스트/vo추가

--- src/test/java/com/pet/ex/controller/MapControllerTest.java
+++ src/test/java/com/pet/ex/controller/MapControllerTest.java
@@ -54,42 +54,24 @@ public class MapControllerTest {
 	 LocalDateTime now = LocalDateTime.now();
      Timestamp timestamp = Timestamp.valueOf(now);//시간test용 데이터
    
-     
-     MemberVO memberVO = new MemberVO();
-     BoardtypeVO boardtypeVO = new BoardtypeVO();
-     GoodsVO goodsVO = new GoodsVO();
-     CategoryVO categoryVO = new CategoryVO();
-
-     
     private MockMvc mockMvc;
     
-    /*
-    MockMvc란? 실제 객체와 비슷하지만 테스트에 필요한 기능만 가지는 가짜 객체를 만들어서
-        애플리케이션 서버에 배포하지 않고도 스프링 MVC 동작을 재현할 수 있는 클래스를 의미.
-        즉 컨트롤러를 테스트할 수있게 만든 가짜 객체(톰캣이 아닌 가상환경을 만들어서 테스트 진행)
-    */
-
-
     @Autowired 
     MapController mapController;
 
    @Before
     public void setup(){//클래스 내에 존재하는 각각의 test를 실행하기 전에 매번 실행한다
 	   this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
-    }
-   
+    }  
    
    	@Test
-	public void mapTest() throws Exception {//mvc가 잘 작동되는지 확인한다. 
-		
+	public void mapTest() throws Exception {//mvc가 잘 작동되는지 확인한다. 	
 		mockMvc.perform(get("/map/home"))//파라미터 값이 없어서 경로만 써주어도 잘 돌아간다.
 			.andReturn()
 			.getModelAndView()
 			.getViewName();
 		
 	}
-   	
-	
    	
    	@Test//이동대상 경로를 검증하는 테스트
 	public void map_viewTest() throws Exception {
@@ -102,10 +84,10 @@ public class MapControllerTest {
 	}
    	
    	
-   	@Test
+   	@Test//write할때 이미지 파일 업로드 때문에 multipart로 보내준다
 	public void map_writeTest() throws Exception {
    		
-   		mockMvc.perform(multipart("/map/write")//write할때 이미지 파일 업로드 때문에 multipart로 보내준다
+   		mockMvc.perform(multipart("/map/write")
    				.file("uploadfile", "테스트".getBytes())
    				.param("member_id", "wjdalsck@hs.ac.kr")
    	   			.param("location", "서울특별시 중구 명동")
